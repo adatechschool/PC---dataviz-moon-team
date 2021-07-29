15abed564d46cf8fde5c010d71c19cd89a74b7fc
Fhommand <85489785+Fhommand@users.noreply.github.com>
2021-07-29 11:49:04 +0200

    m a j

--- src/moon_rise_script.js
+++ src/moon_rise_script.js
@@ -5,23 +5,41 @@ function fetchMoon(){
                     console.log(response);
                     var userData = response;
                     //document.getElementById('earth_hour').innerHTML = response.current_time;
-                    document.getElementById('date_earth').innerHTML = response.date;
-                    document.getElementById('earth_latitude').innerHTML = response.location.latitude;
-                    document.getElementById('earth_longitude').innerHTML = response.location.longitude;   
-                    document.getElementById('moon_azimuth').innerHTML = response.moon_azimuth;
-                    document.getElementById('moon_altitude').innerHTML = response.moon_altitude;   
-                    document.getElementById('moon_distance').innerHTML = response.moon_distance;   
-                    document.getElementById('moonrise').innerHTML = response.moonrise;   
-                    document.getElementById('moonset').innerHTML = response.moonset;   
-                    document.getElementById('solar_noon').innerHTML = response.solar_noon;   
-                    document.getElementById('sun_altitude').innerHTML = response.sun_altitude;   
-                    document.getElementById('sun_azimuth').innerHTML = response.sun_azimuth;   
-                    document.getElementById('sun_distance').innerHTML = response.sun_distance;   
-                    document.getElementById('sunrise').innerHTML = response.sunrise;  
-                    document.getElementById('sunset').innerHTML = response.sunset;   
+                    document.getElementById('date_earth').innerHTML = " " + "DATE ON EARTH : " + response.date;
+                    document.getElementById('earth_latitude').innerHTML =  " "+ "LATITUDE : "+ response.location.latitude; 
+                    document.getElementById('earth_longitude').innerHTML = " "+ "LONGITUDE : "+ response.location.longitude;   
+                    document.getElementById('moon_azimuth').innerHTML = " "  + "MOON AZIMUTH : " + response.moon_azimuth ;
+                    document.getElementById('moon_altitude').innerHTML =  " "+ "MOON ALTITUDE  : " + response.moon_altitude ;   
+                    document.getElementById('moon_distance').innerHTML =  " " + "MOON DISTANCE : " +  response.moon_distance;   
+                    document.getElementById('moonrise').innerHTML =  " " + " MOONRISE : " + response.moonrise;   
+                    document.getElementById('moonset').innerHTML = " " + " MOONSET : " + response.moonset ;   
+                    document.getElementById('solar_noon').innerHTML = " " + " SOLAR NOON : " + response.solar_noon;   
+                    document.getElementById('sun_altitude').innerHTML =  " " + " SUN ALTITUDE : " + response.sun_altitude ;   
+                    document.getElementById('sun_azimuth').innerHTML =  " " + " SUN AZIMUTH : " +response.sun_azimuth; 
+                    document.getElementById('sun_distance').innerHTML = " " + "SUN DISTANCE : " + response.sun_distance ;    
+                    document.getElementById('sunrise').innerHTML = " " + " SUNRISE : " +response.sunrise;  
+                    document.getElementById('sunset').innerHTML = " " + "SUNSET : " + response.sunset;   
+
+            
  
 
                 })
             } 
     fetchMoon();
+
+    var backgroundImage = document.getElementById("background_image");
+
+    var imageArray = ["/full_moon_black.jpg","/full_moon_white.jpg"];
+
+    var imageIndex = 0;
+
+    function changeImage() {
+        backgroundImage.setAttribute('src',imageArray[imageIndex]);
+        imageIndex++;
+        if (imageIndex >= imageArray.length) {
+            imageIndex = 0;
+        }
+    }
+
+    setInterval(changeImage,500);
     
