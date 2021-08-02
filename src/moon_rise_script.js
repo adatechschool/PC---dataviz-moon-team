375534c84c6a810054ec95d117e95139737b3212
Fhommand <85489785+Fhommand@users.noreply.github.com>
2021-08-02 11:17:16 +0200

    mise à jour api + css

--- src/moon_rise_script.js
+++ src/moon_rise_script.js
@@ -1,49 +1,48 @@
-function fetchMoon(){
-    fetch('https://api.ipgeolocation.io/astronomy?apiKey=bc821545b212458b93d35ab006e86be3&lat=48.856&long=2.352',{ method:'GET'})
-                .then((response) => response.json())
-                .then((response) => {
-                    console.log(response);
-                    var userData = response;
-                    //document.getElementById('earth_hour').innerHTML = response.current_time;
-                    document.getElementById('date_earth').innerHTML = " " + "DATE ON EARTH : " + response.date;
-                    document.getElementById('earth_latitude').innerHTML =  " "+ "LATITUDE : "+ response.location.latitude; 
-                    document.getElementById('earth_longitude').innerHTML = " "+ "LONGITUDE : "+ response.location.longitude;   
-                    document.getElementById('moon_azimuth').innerHTML = " "  + "MOON AZIMUTH : " + response.moon_azimuth ;
-                    document.getElementById('moon_altitude').innerHTML =  " "+ "MOON ALTITUDE  : " + response.moon_altitude ;   
-                    document.getElementById('moon_distance').innerHTML =  " " + "MOON DISTANCE : " +  response.moon_distance;   
-                    document.getElementById('moonrise').innerHTML =  " " + " MOONRISE : " + response.moonrise;   
-                    document.getElementById('moonset').innerHTML = " " + " MOONSET : " + response.moonset ;   
-                    document.getElementById('solar_noon').innerHTML = " " + " SOLAR NOON : " + response.solar_noon;   
-                    document.getElementById('sun_altitude').innerHTML =  " " + " SUN ALTITUDE : " + response.sun_altitude ;   
-                    document.getElementById('sun_azimuth').innerHTML =  " " + " SUN AZIMUTH : " +response.sun_azimuth; 
-                    document.getElementById('sun_distance').innerHTML = " " + "SUN DISTANCE : " + response.sun_distance ;    
-                    document.getElementById('sunrise').innerHTML = " " + " SUNRISE : " +response.sunrise;  
-                    document.getElementById('sunset').innerHTML = " " + "SUNSET : " + response.sunset;   
 
-            
- 
+    
+    
+window.onload = function() {
+    var startPos;
 
-                })
-            } 
-    fetchMoon();
+    var fetchMoon = function(latitude, longitude) {
+        let url = 'https://api.ipgeolocation.io/astronomy?apiKey=bc821545b212458b93d35ab006e86be3&lat=' + latitude + '&long=' + longitude;
+        fetch(url, {method:'GET'})
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(response);
+                var userData = response;
+                document.getElementById('date_earth').innerHTML = " " + "DATE ON EARTH : " + response.date;
+                document.getElementById('earth_latitude').innerHTML =  " "+ "LATITUDE : "+ response.location.latitude; 
+                document.getElementById('earth_longitude').innerHTML = " "+ "LONGITUDE : "+ response.location.longitude;   
+                document.getElementById('moon_azimuth').innerHTML = " "  + "MOON AZIMUTH : " + response.moon_azimuth ;
+                document.getElementById('moon_altitude').innerHTML =  " "+ "MOON ALTITUDE  : " + response.moon_altitude ;   
+                document.getElementById('moon_distance').innerHTML =  " " + "MOON DISTANCE : " +  response.moon_distance;   
+                document.getElementById('moonrise').innerHTML =  " " + " MOONRISE : " + response.moonrise;   
+                document.getElementById('moonset').innerHTML = " " + " MOONSET : " + response.moonset ;   
+                document.getElementById('solar_noon').innerHTML = " " + " SOLAR NOON : " + response.solar_noon;   
+                document.getElementById('sun_altitude').innerHTML =  " " + " SUN ALTITUDE : " + response.sun_altitude ;   
+                document.getElementById('sun_azimuth').innerHTML =  " " + " SUN AZIMUTH : " +response.sun_azimuth; 
+                document.getElementById('sun_distance').innerHTML = " " + "SUN DISTANCE : " + response.sun_distance ;    
+                document.getElementById('sunrise').innerHTML = " " + " SUNRISE : " +response.sunrise;  
+                document.getElementById('sunset').innerHTML = " " + "SUNSET : " + response.sunset;
+        })
+    }
 
-    // var backgroundImage = document.getElementById("background_image");
+    var geoSuccess = function(position) {
+        startPos = position;
+        fetchMoon(startPos.coords.latitude, startPos.coords.longitude);
+    };
 
-    // var imageArray = ["/full_moon_black.jpg","/full_moon_white.jpg"];
+    navigator.geolocation.getCurrentPosition(geoSuccess);
+};
 
-    // var imageIndex = 0;
+if (navigator.geolocation) {
+    console.log('Geolocation is supported!');
+} else {
+    console.log('Geolocation is not supported for this Browser/OS.');
+}
 
-    // function changeImage() {
-    //     backgroundImage.setAttribute('src',imageArray[imageIndex]);
-    //     imageIndex++;
-    //     if (imageIndex >= imageArray.length) {
-    //         imageIndex = 0;
-    //     }
-    // }
 
-    // setInterval(changeImage,500);
+    
 
-    function changeImage(element) {
-        var x = element.getElementsByTagName('img').item(0);
-        var v    }
     
