bb7dd677c52a521642eea08f2a6e5407100ea9d8
Fhommand <85489785+Fhommand@users.noreply.github.com>
2021-08-03 14:22:58 +0200

    mise à jour

--- src/moon_rise_script.js
+++ src/moon_rise_script.js
@@ -63,5 +63,15 @@ if (navigator.geolocation) {
             }  
             
            
-             // paramètres à récupérer de la 2e requête 
+             // gestion du dégradé
+
+             $(document).mousemove(function(event) {
+                windowWidth = $(window).width();
+                windowHeight = $(window).height();
+                
+                mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+                mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+                
+                $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+              });
 
